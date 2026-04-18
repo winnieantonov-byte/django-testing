9d7ac177ff28937962fc8ae4f0f2d7d509b164d5
Antonov Astafiy <winnieantonov@gmail.com>
2026-04-18 06:41:58 +0300

    fix

--- ya_news/news/pytest_tests/test_logic.py
+++ ya_news/news/pytest_tests/test_logic.py
@@ -40,11 +40,12 @@ class TestCommentCreation(TestCase):
     def test_user_cant_use_bad_words(self):
         bad_words_data = {'text': f'Какой-то текст, {BAD_WORDS[0]}, еще текст'}
         response = self.auth_client.post(self.url, data=bad_words_data)
+        # Проверяем наличие ошибки в словаре ошибок формы через контекст
         self.assertFormError(
             response,
             'form',
             'text',
-            WARNING
+            errors=WARNING
         )
         comments_count = Comment.objects.count()
         self.assertEqual(comments_count, 0)
