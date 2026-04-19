e7f434a387ba95a6187863f2bb1d2d0c3eaf08ea
Antonov Astafiy <winnieantonov@gmail.com>
2026-04-20 01:27:45 +0300

    fix

--- ya_news/news/pytest_tests/test_logic.py
+++ ya_news/news/pytest_tests/test_logic.py
@@ -2,141 +2,95 @@ import pytest
 from http import HTTPStatus
 
 from django.urls import reverse
+from pytest_django.asserts import assertRedirects, assertFormError
+from pytils.translit import slugify
 
-from news.forms import BAD_WORDS, WARNING
-from news.models import Comment, News
-
-
-@pytest.fixture
-def news(db):
-    return News.objects.create(title='Заголовок', text='Текст')
+from notes.forms import WARNING
+from notes.models import Note
 
 
-@pytest.fixture
-def author(db, django_user_model):
-    return django_user_model.objects.create(username='Автор комментария')
+@pytest.mark.django_db
+def test_user_can_create_note(author_client, author, form_data):
+    url = reverse('notes:add')
+    response = author_client.post(url, data=form_data)
+    assertRedirects(response, reverse('notes:success'))
+    assert Note.objects.count() == 1
+    new_note = Note.objects.get()
+    assert new_note.title == form_data['title']
+    assert new_note.text == form_data['text']
+    assert new_note.slug == form_data['slug']
+    assert new_note.author == author
 
 
-@pytest.fixture
-def reader(db, django_user_model):
-    return django_user_model.objects.create(username='Читатель')
+@pytest.mark.django_db
+def test_anonymous_user_cant_create_note(client, form_data):
+    url = reverse('notes:add')
+    response = client.post(url, data=form_data)
+    login_url = reverse('users:login')
+    expected_url = f'{login_url}?next={url}'
+    assertRedirects(response, expected_url)
+    assert Note.objects.count() == 0
 
 
-@pytest.fixture
-def comment(news, author):
-    return Comment.objects.create(
-        news=news,
-        author=author,
-        text='Текст комментария'
+@pytest.mark.django_db
+def test_not_unique_slug(author_client, note, form_data):
+    url = reverse('notes:add')
+    form_data['slug'] = note.slug
+    response = author_client.post(url, data=form_data)
+    assertFormError(
+        response,
+        'form',
+        'slug',
+        note.slug + WARNING
     )
-
-
-@pytest.fixture
-def auth_client(reader, client):
-    client.force_login(reader)
-    return client
-
-
-@pytest.fixture
-def author_client(author, client):
-    client.force_login(author)
-    return client
-
-
-@pytest.fixture
-def reader_client(reader, client):
-    client.force_login(reader)
-    return client
-
-
-@pytest.fixture
-def url_detail(news):
-    return reverse('news:detail', args=(news.id,))
-
-
-@pytest.fixture
-def url_to_comments(url_detail):
-    return url_detail + '#comments'
-
-
-@pytest.fixture
-def edit_url(comment):
-    return reverse('news:edit', args=(comment.id,))
-
-
-@pytest.fixture
-def delete_url(comment):
-    return reverse('news:delete', args=(comment.id,))
+    assert Note.objects.count() == 1
 
 
 @pytest.mark.django_db
-def test_anonymous_user_cant_create_comment(client, url_detail):
-    form_data = {'text': 'Текст комментария'}
-    client.post(url_detail, data=form_data)
-    assert Comment.objects.count() == 0
+def test_empty_slug(author_client, form_data):
+    url = reverse('notes:add')
+    form_data.pop('slug')
+    response = author_client.post(url, data=form_data)
+    assertRedirects(response, reverse('notes:success'))
+    assert Note.objects.count() == 1
+    new_note = Note.objects.get()
+    expected_slug = slugify(form_data['title'])
+    assert new_note.slug == expected_slug
 
 
 @pytest.mark.django_db
-def test_user_can_create_comment(
-    auth_client, url_detail, url_to_comments, news, reader
-):
-    text = 'Текст комментария'
-    form_data = {'text': text}
-    response = auth_client.post(url_detail, data=form_data)
-    assert response.status_code == HTTPStatus.FOUND
-    assert response.url == url_to_comments
-    assert Comment.objects.count() == 1
-    comment = Comment.objects.get()
-    assert comment.text == text
-    assert comment.news == news
-    assert comment.author == reader
+def test_author_can_edit_note(author_client, form_data, note):
+    url = reverse('notes:edit', args=(note.slug,))
+    response = author_client.post(url, form_data)
+    assertRedirects(response, reverse('notes:success'))
+    note.refresh_from_db()
+    assert note.title == form_data['title']
+    assert note.text == form_data['text']
+    assert note.slug == form_data['slug']
 
 
 @pytest.mark.django_db
-def test_user_cant_use_bad_words(auth_client, url_detail):
-    bad_words_data = {'text': f'Текст с {BAD_WORDS[0]}'}
-    response = auth_client.post(url_detail, data=bad_words_data)
-    form = response.context['form']
-    assert WARNING in form.errors['text']
-    assert Comment.objects.count() == 0
-
-
-@pytest.mark.django_db
-def test_author_can_delete_comment(author_client, delete_url, url_to_comments):
-    response = author_client.delete(delete_url)
-    assert response.status_code == HTTPStatus.FOUND
-    assert response.url == url_to_comments
-    assert Comment.objects.count() == 0
-
-
-@pytest.mark.django_db
-def test_user_cant_delete_comment_of_another_user(reader_client, delete_url):
-    response = reader_client.delete(delete_url)
+def test_other_user_cant_edit_note(not_author_client, form_data, note):
+    url = reverse('notes:edit', args=(note.slug,))
+    response = not_author_client.post(url, form_data)
     assert response.status_code == HTTPStatus.NOT_FOUND
-    assert Comment.objects.count() == 1
+    note_from_db = Note.objects.get(id=note.id)
+    assert note.title == note_from_db.title
+    assert note.text == note_from_db.text
+    assert note.slug == note_from_db.slug
 
 
 @pytest.mark.django_db
-def test_author_can_edit_comment(
-    author_client, edit_url, url_to_comments, comment
-):
-    new_text = 'Обновлённый комментарий'
-    form_data = {'text': new_text}
-    response = author_client.post(edit_url, data=form_data)
-    assert response.status_code == HTTPStatus.FOUND
-    assert response.url == url_to_comments
-    comment.refresh_from_db()
-    assert comment.text == new_text
+def test_author_can_delete_note(author_client, note):
+    url = reverse('notes:delete', args=(note.slug,))
+    response = author_client.post(url)
+    assertRedirects(response, reverse('notes:success'))
+    assert Note.objects.count() == 0
 
 
 @pytest.mark.django_db
-def test_user_cant_edit_comment_of_another_user(
-    reader_client, edit_url, comment
-):
-    old_text = comment.text
-    new_text = 'Обновлённый комментарий'
-    form_data = {'text': new_text}
-    response = reader_client.post(edit_url, data=form_data)
+def test_other_user_cant_delete_note(not_author_client, note):
+    url = reverse('notes:delete', args=(note.slug,))
+    response = not_author_client.post(url)
     assert response.status_code == HTTPStatus.NOT_FOUND
-    comment.refresh_from_db()
-    assert comment.text == old_text
+    assert Note.objects.count() == 1
