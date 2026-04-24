f430e99dbd66628aba1ada67b9725e6852efa25a
Antonov Astafiy <winnieantonov@gmail.com>
2026-04-24 13:35:50 +0300

    fix

--- ya_news/news/pytest_tests/test_content.py
+++ ya_news/news/pytest_tests/test_content.py
@@ -4,11 +4,11 @@ from django.conf import settings
 from news.forms import CommentForm
 
 
-@pytest.mark.django_db
-def test_news_count(client, home_url, news_list):
-    response = client.get(home_url)
-    object_list = response.context['object_list']
-    assert len(object_list) == settings.NEWS_COUNT_ON_HOME_PAGE
+@pytest.mark.django_db 
+def test_news_count(client, home_url, news_list): 
+    assert len( 
+        client.get(home_url).context['object_list']
+    ) == settings.NEWS_COUNT_ON_HOME_PAGE
 
 
 @pytest.mark.django_db
